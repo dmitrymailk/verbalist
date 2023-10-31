42a9dbcc3d48f446d774496dc5868c6339075699
dmitrymailk <dmitrymail_k@mail.ru>
2023-11-01 01:24:00 +0300

    add simple superglue evaluation

--- verbalist/datasets/slim_orca/translate.sh
+++ verbalist/datasets/slim_orca/translate.sh
@@ -1,6 +1,6 @@
 # export CUDA_VISIBLE_DEVICES="1,2,3"
-export CUDA_VISIBLE_DEVICES="2,3"
-# export CUDA_VISIBLE_DEVICES="0,1"
+# export CUDA_VISIBLE_DEVICES="2,3"
+export CUDA_VISIBLE_DEVICES="0,1"
 
 nohup python -u slim_orca.py > ./logs/translation.log &
 # nohup python -u slim_orca.py > ./logs/translation_2.log &
