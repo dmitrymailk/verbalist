b57df8e8455c2f3b95ed51d4f07deb0a271747c9
dmitrymailk <dmitrymail_k@mail.ru>
2023-10-31 15:26:03 +0300

    add slimorca translation script

--- verbalist/datasets/slim_orca/slim_orca.py
+++ verbalist/datasets/slim_orca/slim_orca.py
@@ -1,6 +1,5 @@
 
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
+
 
 from sqlalchemy.orm import DeclarativeBase, Session
 from sqlalchemy import Column, Integer, String
@@ -10,6 +9,8 @@ from datasets import load_dataset, Dataset
 from sqlalchemy.orm import DeclarativeBase, Session
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
+from sqlalchemy.pool import SingletonThreadPool
+from sqlalchemy import text
 
 import torch
 dataset = load_dataset("dim/SlimOrcaEN")
@@ -18,8 +19,11 @@ dataset = dataset.to_list()
 
 
 engine = create_engine(
-    "sqlite:////home/kosenko/verbalist/verbalist/datasets/slim_orca/slim_orca.db"
+    open("./postgre_creds").read()
 )
+
+# with engine.begin() as conn:
+#     conn.execute(text('PRAGMA journal_mode=WAL'))
 
 
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
@@ -29,7 +33,11 @@ model = AutoModelForSeq2SeqLM.from_pretrained(
     device_map="auto",
     torch_dtype=torch.float16,
     # load_in_4bit=True
+    # use_flash_attention_2=True
 )
+model.eval()
+# model = torch.compile(model)
+
 tokenizer = AutoTokenizer.from_pretrained(
     "facebook/wmt21-dense-24-wide-en-x",
 )
@@ -59,60 +67,63 @@ class SlimOrcaTranslation(Base):
 
 # создаем таблицы
 Base.metadata.create_all(bind=engine)
+with torch.no_grad():
+    # создаем сессию подключения к бд
+    with Session(autoflush=False, bind=engine) as db:
+        # for key in tqdm(range(3_00_000, len(dataset))):
+        for key in tqdm(range(1_00_000, len(dataset))):
+        # for key in tqdm(range(10000, len(dataset))):
+            trans = (
+                db.query(SlimOrcaTranslation).filter(SlimOrcaTranslation.key == key).first()
+            )
+            # print(trans)
+            if trans is None:
+                try:
+                    item = dataset[key]
+                    print(f"KEY={key}")
+                    for i in range(len(item["conversations"])):
+                        value_en = item["conversations"][i]["value"].split("\n")
+                        value_en = [item for item in value_en if len(item) > 0]
+                        # print(item['conversations'][i]['value'])
+                        value_ru = tokenizer.batch_encode_plus(
+                            value_en,
+                            return_tensors="pt",
+                            padding=True,
+                        ).to("cuda")
 
-# создаем сессию подключения к бд
-with Session(autoflush=False, bind=engine) as db:
-    for key in tqdm(range(len(dataset))):
-        trans = (
-            db.query(SlimOrcaTranslation).filter(SlimOrcaTranslation.key == key).first()
-        )
-        # print(trans)
-        if trans is None:
-            try:
-                item = dataset[key]
-                print(f"KEY={key}")
-                for i in range(len(item["conversations"])):
-                    value_en = item["conversations"][i]["value"].split("\n")
-                    value_en = [item for item in value_en if len(item) > 0]
-                    # print(item['conversations'][i]['value'])
-                    value_ru = tokenizer.batch_encode_plus(
-                        value_en,
-                        return_tensors="pt",
-                        padding=True,
-                    ).to("cuda")
-
-                    value_ru = model.generate(
-                        **value_ru,
-                        forced_bos_token_id=tokenizer.get_lang_id("ru"),
-                        # num_beams=2
+                        value_ru = model.generate(
+                            **value_ru,
+                            forced_bos_token_id=tokenizer.get_lang_id("ru"),
+                            # num_beams=2
+                        )
+                        value_ru = tokenizer.batch_decode(
+                            value_ru,
+                            skip_special_tokens=True,
+                        )
+                        print(f"VALUE_EN={value_en}")
+                        value_ru = "\n".join(value_ru)
+                        item["conversations"][i]["value_ru"] = value_ru
+                        print(f"VALUE_RU={value_ru}")
+                        print("-" * 10)
+                        print("-" * 10)
+                    # break
+                    # создаем объект Person для добавления в бд
+                    row = SlimOrcaTranslation(
+                        key=key,
+                        json=json.dumps(
+                            item,
+                            ensure_ascii=False,
+                        ),
                     )
-                    value_ru = tokenizer.batch_decode(
-                        value_ru,
-                        skip_special_tokens=True,
+                    db.add(row)  # добавляем в бд
+                    db.commit()
+                except:
+                    row = SlimOrcaTranslation(
+                        key=key,
+                        json="",
                     )
-                    print(f"VALUE_EN={value_en}")
-                    value_ru = "\n".join(value_ru)
-                    item["conversations"][i]["value_ru"] = value_ru
-                    print(f"VALUE_RU={value_ru}")
-                    print("-" * 10)
-                    print("-" * 10)
-                # break
-                # создаем объект Person для добавления в бд
-                row = SlimOrcaTranslation(
-                    key=key,
-                    json=json.dumps(
-                        item,
-                        ensure_ascii=False,
-                    ),
-                )
-                db.add(row)  # добавляем в бд
-                db.commit()
-            except:
-                row = SlimOrcaTranslation(
-                    key=key,
-                    json="",
-                )
-                db.add(row)  # добавляем в бд
-                db.commit()
+                    db.add(row)  # добавляем в бд
+                    db.commit()
+                    
 
-# dataset[0]
+    # dataset[0]
--- verbalist/datasets/slim_orca/translate.sh
+++ verbalist/datasets/slim_orca/translate.sh
@@ -1,1 +1,7 @@
+# export CUDA_VISIBLE_DEVICES="1,2,3"
+export CUDA_VISIBLE_DEVICES="2,3"
+# export CUDA_VISIBLE_DEVICES="0,1"
+
 nohup python -u slim_orca.py > ./logs/translation.log &
+# nohup python -u slim_orca.py > ./logs/translation_2.log &
+# nohup python -u slim_orca.py > ./logs/translation_3.log &
