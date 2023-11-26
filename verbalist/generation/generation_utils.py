4cf7d7e4c495706037a95ed30556d88559081871
dmitrymailk <dmitrymail_k@mail.ru>
2023-11-27 01:49:04 +0300

    add openchat_verbalist generation util

--- verbalist/generation/generation_utils.py
+++ verbalist/generation/generation_utils.py
@@ -41,6 +41,41 @@ class VerbalistConversation:
         return final_text.strip()
 
 
+class VerbalistOpenchatConversation:
+    def __init__(
+        self,
+        message_template="<s> {role}\n{content} </s> \n",
+        system_prompt="Ты — Буквоед, русскоязычный автоматический ассистент. Ты разговариваешь с людьми и помогаешь им.",
+        start_token_id=1,
+        bot_token_id=12435,
+    ):
+        self.message_template = message_template
+        self.start_token_id = start_token_id
+        self.bot_token_id = bot_token_id
+        self.messages = [{"role": "system ", "content": system_prompt}]
+
+    def get_start_token_id(self):
+        return self.start_token_id
+
+    def get_bot_token_id(self):
+        return self.bot_token_id
+
+    def add_user_message(self, message):
+        self.messages.append({"role": "user high_quality", "content": message})
+
+    def add_bot_message(self, message):
+        self.messages.append({"role": "bot high_quality", "content": message})
+
+    def get_prompt(self, tokenizer):
+        final_text = ""
+        for message in self.messages:
+            message_text = self.message_template.format(**message)
+            final_text += message_text
+        final_text += "<s> bot high_quality"
+
+        return final_text.strip()
+
+
 def generate(model, tokenizer, prompt, generation_config):
     data = tokenizer(
         prompt,
